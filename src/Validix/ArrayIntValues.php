53bb0af246a752171c72b544f3af17a291d0b7ed
Carlos Cardoso <carloscardoso@blixdigital.com>
2020-11-07 18:17:12 +0000

    Updated ArrayIntValues Validator to check for empty arrays

--- src/Validix/ArrayIntValues.php
+++ src/Validix/ArrayIntValues.php
@@ -9,11 +9,13 @@ class ArrayIntValues
     extends AbstractValidator
         implements ValidatorInterface
 {
-    const MSG_NOT_ARRAY = 'msgNotArray';
-    const MSG_NOT_VALID = 'msgNotValid';
+    const MSG_NOT_ARRAY     = 'msgNotArray';
+    const MSG_EMPTY_ARRAY   = 'msgEmptyArray';
+    const MSG_NOT_VALID     = 'msgNotValid';
 
     protected $_messageTemplates = array(
         self::MSG_NOT_ARRAY       => "'%value%' is not a array",
+        self::MSG_EMPTY_ARRAY     => "'%value%' is empty array",
         self::MSG_NOT_VALID       => "'%value%' is not a valid array with integers",
     );
 
@@ -24,6 +26,11 @@ class ArrayIntValues
 
         if ( ! is_array($value) ) {
             $this->error(self::MSG_NOT_ARRAY);
+            return false;
+        }
+
+        if ( !$value ) {
+            $this->error(self::MSG_EMPTY_ARRAY);
             return false;
         }
 
