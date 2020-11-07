b1c421da8f9eb5cdd51bfaed3bc2c68eb298e9e0
Carlos Cardoso <carloscardoso@blixdigital.com>
2020-11-07 17:58:45 +0000

    Added validator to check if value is an array and it's values are integers

---
+++ src/Validix/ArrayIntValues.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Validix;
+
+use Zend\Validator\AbstractValidator;
+use Zend\Validator\ValidatorInterface;
+
+class ArrayIntValues
+    extends AbstractValidator
+        implements ValidatorInterface
+{
+    const MSG_NOT_ARRAY = 'msgNotArray';
+    const MSG_NOT_VALID = 'msgNotValid';
+
+    protected $_messageTemplates = array(
+        self::MSG_NOT_ARRAY       => "'%value%' is not a array",
+        self::MSG_NOT_VALID       => "'%value%' is not a valid array with integers",
+    );
+
+    public function isValid($value)
+    {
+
+        $this->setValue($value);
+
+        if ( ! is_array($value) ) {
+            $this->error(self::MSG_NOT_ARRAY);
+            return false;
+        }
+
+        foreach ($value as $elementValue ) {
+
+            if ( is_string($elementValue) ) {
+
+                if ( ctype_digit($elementValue) ) {
+
+                    continue;
+                }
+
+            } else {
+
+                if ( is_int($elementValue) ) {
+                    continue;
+                }
+            }
+
+            $this->error(self::MSG_NOT_VALID);
+            return false;
+        }
+
+        return true;
+
+    }
+
+
+}
