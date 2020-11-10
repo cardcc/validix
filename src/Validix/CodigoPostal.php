f7c234cb4337ec8f0b7ea5371ee43746be7d4509
Carlos Cardoso <carloscardoso@blixdigital.com>
2020-11-10 10:40:51 +0000

    Added Codigo Postal and Sequencia Postal Validators

---
+++ src/Validix/CodigoPostal.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Validix;
+
+use Zend\Validator\AbstractValidator;
+use Zend\Validator\ValidatorInterface;
+
+class CodigoPostal
+    extends AbstractValidator
+        implements ValidatorInterface
+{
+    const MSG_INVALID   = 'msgInvalid';
+
+    protected $_messageTemplates = array(
+        self::MSG_INVALID       => "'%value%' is not a valid Codigo Postal",
+    );
+
+    public function isValid($value)
+    {
+
+        $this->setValue($value);
+        $value = (string) $value;
+
+        if ( preg_match('/^[0-9]{4}$/D',$value) ) {
+
+            $this->error(self::MSG_INVALID);
+            return false;
+        }
+
+        return true;
+
+    }
+
+}
---
+++ src/Validix/SequenciaPostal.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Validix;
+
+use Zend\Validator\AbstractValidator;
+use Zend\Validator\ValidatorInterface;
+
+class SequenciaPostal
+    extends AbstractValidator
+        implements ValidatorInterface
+{
+    const MSG_INVALID   = 'msgInvalid';
+
+    protected $_messageTemplates = array(
+        self::MSG_INVALID       => "'%value%' is not a valid Sequencia Postal",
+    );
+
+    public function isValid($value)
+    {
+
+        $this->setValue($value);
+        $value = (string) $value;
+
+        if ( preg_match('/^[0-9]{3}$/D',$value) ) {
+            
+            $this->error(self::MSG_INVALID);
+            return false;
+        }
+
+        return true;
+
+    }
+
+}
